54dcfe15f9ca36a8df92dda696c087291bc241ba
len-IT <118350081+len-IT@users.noreply.github.com>
2026-04-03 08:40:41 +0700

    Revert "Add learning and progress module"

--- backend/routes/learningRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { protect } from '../middlewares/authMiddleware.js';
-import { getCourseLessons } from '../controllers/learningController.js';
-
-const router = express.Router();
-
-router.get('/course/:courseId', protect, getCourseLessons);
-
-export default router;
